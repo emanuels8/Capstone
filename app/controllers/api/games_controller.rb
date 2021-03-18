5da004dc5e7f35e7b4e3af65b3181d85d40a4276
Emanuel Sanchez <emmanuels8001@gmail.com>
2021-03-17 21:17:36 -0500

    NICEgit add --allgit add --all WORKING Reviews

--- app/controllers/api/games_controller.rb
+++ app/controllers/api/games_controller.rb
@@ -6,21 +6,24 @@ class Api::GamesController < ApplicationController
 
   def show
     @genre_name = []
+    @review_info = []
+    @array = []
+    @info = {}
     @genre = Game.find_by(id: params["id"]).game_genre.all.map { |genre|
       @genre_name << genre.genre.name
     }
-
+    @test = Review.where(game_id: params["id"])
     @game = Game.find_by(id: params["id"])
-    @review_info = Game.find_by(id: params["id"]).review
+    if Game.first.id == Review.first.game_id
+      Review.where(game_id: params["id"]).all.map { |review|
+        (@info[:username] = review.user.username)
+        (@info[:description] = review.description)
+        (@info[:rating] = review.rating)
+        @array.push(@info)
+        @info = {}
+      }
+    end
 
-    @review_usernames = Review.all.map { |review|
-      if @game.id == review.game_id
-        review.user.username
-      end
-    }
-    @review_usernames = @review_usernames.compact
     render "show.json.jb"
   end
 end
-
-# Game.first.game_genre.first.genre.name
